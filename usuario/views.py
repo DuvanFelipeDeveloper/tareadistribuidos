7973bc9d53e2908bd00b3481c57332add1f118a2
Duvan <duvan@zaita.com.co>
2022-12-21 15:39:54 -0500

    archivo desde google

--- usuario/views.py
+++ usuario/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from .models import Producto 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import requests
+
+
 
 # Create your views here.
 
 def home(request):
+
+    url = "https://drive.google.com/uc?id=1OuUHXjkP-Si-F8IaynyCebgIRsHDRgKH&export=download&authuser=0"
+
+    response= requests.get(url)
+
+    print(response.text)
+    
     productoslistados =Producto.objects.all()
-    return render(request,"lista.html", {"productos": productoslistados})
+    return render(request,"lista.html", {"productos": productoslistados, "titulo": response.text})
+    
+    
